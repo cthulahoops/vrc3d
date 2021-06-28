cab653f6eaf4da48f88e8d30f0699f0858d92e60
Adam Kelly <adam@cthulahoops.org>
2021-06-28 09:29:30 +0100

    Clean up shader.py.

--- shader.py
+++ shader.py
@@ -6,17 +6,11 @@ from matrix import Matrix
 from vector import Vector
 
 
-class Shader_error(Exception):
-    def __init__(self, message):
-        self.message = message
+def create_shader(shader_type, source_path):
+    target = gl.glCreateShader(shader_type)
 
-
-def create_shader(target, source_path):
-    # read shader source
-
-    source_file = open(source_path, "rb")
-    source = source_file.read()
-    source_file.close()
+    with open(source_path, "rb") as source_file:
+        source = source_file.read()
 
     source_length = ctypes.c_int(len(source) + 1)
     source_buffer = ctypes.create_string_buffer(source)
@@ -40,26 +34,20 @@ def create_shader(target, source_path):
     gl.glGetShaderInfoLog(target, log_length, None, log_buffer)
 
     if log_length:
-        raise Shader_error(str(log_buffer.value))
+        raise ValueError(f"SHADER ERROR: {log_buffer.value}")
+
+    return target
 
 
 class Shader:
     def __init__(self, vert_path, frag_path):
         self.program = gl.glCreateProgram()
 
-        # create vertex shader
-
-        self.vert_shader = gl.glCreateShader(gl.GL_VERTEX_SHADER)
-        create_shader(self.vert_shader, vert_path)
+        self.vert_shader = create_shader(gl.GL_VERTEX_SHADER, vert_path)
         gl.glAttachShader(self.program, self.vert_shader)
 
-        # create fragment shader
-
-        self.frag_shader = gl.glCreateShader(gl.GL_FRAGMENT_SHADER)
-        create_shader(self.frag_shader, frag_path)
+        self.frag_shader = create_shader(gl.GL_FRAGMENT_SHADER, frag_path)
         gl.glAttachShader(self.program, self.frag_shader)
-
-        # link program and clean up
 
         gl.glLinkProgram(self.program)
 
@@ -86,15 +74,6 @@ class Shader:
             gl.glUniform3f(location, value.x, value.y, value.z)
         else:
             gl.glUniform1i(location, value)
-
-
-    def uniform_matrix(self, location, matrix):
-        gl.glUniformMatrix4fv(
-            location, 1, gl.GL_FALSE, (gl.GLfloat * 16)(*sum(matrix.data, []))
-        )
-
-    def uniform_vec3(self, location, vec):
-        gl.glUniform3f(location, vec.x, vec.y, vec.z)
 
     def use(self):
         gl.glUseProgram(self.program)
